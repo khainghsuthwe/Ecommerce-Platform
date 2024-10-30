6ea51b3d632cdcb4dc95fcf43f4777b8f987e568
atom017 <khaingjar@gmail.com>
2024-10-30 16:20:11 +0630

    updated user types as global

--- backend/src/types/express.d.ts
+++ backend/src/types/express.d.ts
@@ -5,9 +5,9 @@ declare global {
     namespace Express {
         interface Request {
             user?: {
-                id: string;  // or the appropriate type for your user ID
-                role: string; // or whatever properties you have in the token
-                // Add any other user properties you may have
+                id: string;  
+                role: string; 
+             
             };
         }
     }
