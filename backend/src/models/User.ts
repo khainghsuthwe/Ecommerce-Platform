b4540ff41060c393b5afb29dda69904c6c2ae0a6
atom017 <khaingjar@gmail.com>
2024-10-30 16:18:09 +0630

    [update] update User Model

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -3,11 +3,14 @@ import mongoose, { Document, Schema } from 'mongoose';
 interface IUser extends Document {
     email: string;
     password: string;
+    role: 'user' | 'admin'; 
 }
 
 const userSchema: Schema = new Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    role: { type: String, enum: ['user', 'admin'], default: 'user' }, // Add role field with default value
 });
+
 
 export default mongoose.model<IUser>('User', userSchema);
