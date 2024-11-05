ee232804f61d7186449acab562f65305de1e5cfe
atom017 <khaingjar@gmail.com>
2024-11-05 14:43:32 +0630

    [update] added dotenv config to stripe

--- backend/src/config/stripe.ts
+++ backend/src/config/stripe.ts
@@ -1,5 +1,8 @@
 // src/config/stripe.ts
 import Stripe from 'stripe';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '', {
     apiVersion: '2024-09-30.acacia', // Use an appropriate API version
