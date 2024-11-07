06f1015b55a41e91f5a2c4a7c9c117d24e88304f
atom017 <khaingjar@gmail.com>
2024-11-07 16:02:40 +0630

    [update] update cartRoutes: remove comments

--- backend/src/routes/cartRoutes.ts
+++ backend/src/routes/cartRoutes.ts
@@ -1,17 +1,14 @@
 // src/routes/cartRoutes.ts
 import { Router } from 'express';
 import { addToCart, removeFromCart, viewCart } from '../controllers/cartController';
-//import { checkout } from '../controllers/paymentController';
-import authMiddleware from '../middlewares/authMiddleware'; // Import auth middleware
+import authMiddleware from '../middlewares/authMiddleware'; 
 
 const router = Router();
 
-// Apply auth middleware to the cart routes
-router.use(authMiddleware); // This will protect all routes below
+router.use(authMiddleware); 
 
 router.post('/add', addToCart);
 router.delete('/remove/:productId', removeFromCart);
 router.get('/view', viewCart);
-//router.post('/checkout', checkout); // Use the checkout function from the payment controller
 
 export default router;
