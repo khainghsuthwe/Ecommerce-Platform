456ef01a0c4e038f4f393fe3e22ac56bacaeb745
atom017 <khaingjar@gmail.com>
2024-11-07 16:01:57 +0630

    [update] update Product model: iclude tags field

--- backend/src/models/Product.ts
+++ backend/src/models/Product.ts
@@ -1,4 +1,3 @@
-// src/models/Product.ts
 import mongoose, { Schema, Document } from 'mongoose';
 
 export interface IProduct extends Document {
@@ -6,19 +5,34 @@ export interface IProduct extends Document {
     description: string;
     price: number;
     inventory: number;
-    category: mongoose.Types.ObjectId; 
-    image?: string; 
+    category: mongoose.Types.ObjectId;
+    image?: string;
+    tags: ProductTags[]; 
+}
+
+// Define the tags enum for product tagging
+export enum ProductTags {
+    NEW = "new",
+    POPULAR = "popular",
+    SALE = "sale",
+    FEATURED = "featured",
+    LIMITED = "limited",
+    DISCOUNT = "discount"
 }
 
 const productSchema: Schema<IProduct> = new Schema({
     name: { type: String, required: true },
     description: { type: String, required: true },
-    price: { type: Number, required: true,min: 0 },
-    inventory: { type: Number, required: true,min: 0 },
+    price: { type: Number, required: true, min: 0 },
+    inventory: { type: Number, required: true, min: 0 },
     category: { type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true, index: true },
-    image: { type: String }, 
+    image: { type: String },
+    tags: {
+        type: [String], 
+        enum: Object.values(ProductTags), // Ensure the tags are valid enum values
+        default: [] // Default to an empty array if no tags are provided
+    },
 }, { timestamps: true });
 
 const Product = mongoose.model<IProduct>('Product', productSchema);
 export default Product;
-
